8068bad66567f1ad6bc95632f9879066d2abb04a
Jaewon-Hong <hongjaewon0428@gmail.com>
2026-01-27 20:08:54 +0900

    week05_미로찾기 알고리즘 추가

---
+++ week05/미로탐색_2178.py
@@ -0,0 +1,32 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+
+graph = []
+for _ in range(N) :
+    graph.append(list(map(int, sys.stdin.readline().strip())))
+
+dx = [-1,1,0,0] # 좌우
+dy = [0,0,-1,1] # 상하
+
+queue = [[0,0]]
+
+while queue :
+    x, y = queue.pop(0) 
+    # 최초 queue.pop(0) 동작시 [0,0]이 x에 0, y는 0으로 각각 담김  
+
+    for i in range(4) : # [-1,0] -> [1,0] -> [0,-1] -> [0,1] (상 -> 하 -> 좌 -> 우)
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if nx < 0 or nx >= N or ny < 0 or ny >= M : # 테두리 벗어나면 무시
+           continue
+
+        if graph[nx][ny] == 0: # 벽 무시 
+            continue
+
+        if graph[nx][ny] == 1:
+            graph[nx][ny] = graph[x][y] + 1 # 이전 좌표에서 +1 받는 형태
+            queue.append([nx, ny])
+
+print(graph[N-1][M-1])
